e2b2f65e273e52859c8ad9170c2ddf9a7eab4942
carlosmaeda3d <carlosmaeda3d@yahoo.com>
2024-02-20 20:17:27 -0500

    commit2

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# import pandas as pd
+
 numColumns = 0
 columnNames = []
 columnTypes = []
@@ -5,6 +7,9 @@ numRows = 0
 maxColumns = 10
 maxRows = 200
 
+#Random Generator - generates data to a .csv based on the amount of columns/rows, column names & types, you input
+
+#Asking how many columns
 print('How many columns you want? Max is ' + str(maxColumns))
 numColumns = input()
 
@@ -17,11 +22,20 @@ while int(numColumns) > maxColumns:
 print('How many rows you want to create? Max is ' + str(maxRows))
 numRows = input()
 
+#Asking how many rows
 while int(numRows) > maxRows:
     print('Went over maximum amount of rows. Please try again')
     numRows = 0
     print('How many rows you want to create? Max is ' + str(maxRows))
     numRows = input()
+
+#Naming each column and the type. For string types, make a list that the generator will use
+
+#Random Generator
+
+#Adding generator lists to a panda dataframe
+
+#Export out as a .csv
 
 #  TEST
 print(numColumns)
