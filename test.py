b08f6e564d0b2f1aa621a174ca674f4e753daf47
carlosmaeda3d <carlosmaeda3d@yahoo.com>
2024-02-26 17:35:36 -0500

    commit5

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-# import pandas as pd
+import random
 
-numCols = 0
-colNames = []
-colTypes = []
-colRange = []
-numRows = 0
+numCols = 3
+colNames = ['test', 'test2', 'test3']
+colTypes = ['Int', 'String', 'Boolean']
+colRange = ['0-100', ['rest', ' crest', ' fest'], ['True', 'False']]
+numRows = 8
 maxCols = 10
 maxRows = 200
